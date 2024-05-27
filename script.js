9d8839a199bda73f7d8d24bf0dc931d37100cdbc
greyzeids <64230328+greyzeids@users.noreply.github.com>
2024-05-27 23:22:05 +0200

    Card creation

---
+++ script.js
@@ -0,0 +1,27 @@
+function generateCard() {
+    let suits = ["♦", "♥", "♠", "♣"];
+    let values = [
+        "A",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "J",
+        "Q",
+        "K",
+    ];
+
+    let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    let randomValue = values[Math.floor(Math.random() * values.length)];
+
+    let color = randomSuit === "♦" || randomSuit === "♥" ? "red" : "black";
+
+    document.getElementById("number").innerHTML = `<span style="color: ${color};">${randomValue}</span>`; // prettier-ignore
+    document.getElementById("topLeft").innerHTML = `<span style="color: ${color};">${randomSuit}</span>`; // prettier-ignore
+    document.getElementById("bottomRight").innerHTML = `<span style="color: ${color};">${randomSuit}</span>`; // prettier-ignore
+}
